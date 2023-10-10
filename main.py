b1cabde39ee6b3e6caacd7d1589c39aa4cdec9bc
PlumNoseBear <46938139+PlumNoseBear@users.noreply.github.com>
2023-10-10 00:40:38 +0000

    Добавлено скачивание файлов

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
-from flask import Flask, render_template, request, make_response, flash, redirect, send_from_directory, url_for, session
+from flask import Flask, render_template, request, make_response, flash, redirect, send_from_directory, send_file, url_for, session
 import os
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = 'static/files' # папка для загруженных файлов
-# расширения файлов, которые можно загружать
-ALLOWED_EXTENSIONS = {'h5', 'pt', 'png', 'jpg', 'jpeg', 'mp4', 'avi'}
+from flask_uploads import UploadSet, configure_uploads, DEFAULTS
+
+UPLOADFOLDER = 'static/files' # папка для загруженных файлов
+# расширения файлов, которые можно загружать и воспроизводить
+ALLOWED_EXTENSIONS = {'h5', 'pt', 'png', 'jpg', 'jpeg', 'mp4', 'mov', 'avi'}
+VIDEO = {'mp4', 'mov','avi'}
+VIDEOS = UploadSet('files', DEFAULTS)
+VIDEOS.extensions = ('mp4', 'mov', 'avi')
+
 # создаем экземпляр приложения
 app = Flask(__name__)
 # конфигурируем
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['UPLOADED_FILES_DEST'] = UPLOADFOLDER 
+#app.config['UPLOADFOLDER'] = UPLOADFOLDER
 app.secret_key = 'Scatman"s_world'
+configure_uploads(app, VIDEOS)
+
 
 def allowed_file(filename):
     """ Функция проверки расширения файла """
@@ -35,14 +44,17 @@ def upload_file():
             flash('Сохраняю файл')
             redirect(request.url)
             # сохраняем файл
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
-            return redirect(url_for('/'))
-    play_files = os.listdir(app.config['UPLOAD_FOLDER'])
-    return render_template('index.html', files=play_files)
+            file.save(os.path.join(app.config['UPLOADED_FILES_DEST'], file.filename))
+            #return redirect(url_for('success'), file=file)
+    all_files = os.listdir(app.config['UPLOADED_FILES_DEST'])
+    video_files = filter(lambda x: x.split('.')[1] in VIDEO, all_files)
+    dload_files = filter(lambda x: x.split('.')[1] in ALLOWED_EXTENSIONS, all_files)
+    return render_template('index.html', v_files=video_files, d_files=dload_files)
     
-  
-def download_file():
-  ...
+@app.route('/download/<filename>')
+def download(filename):
+    # Возврат файла для скачивания
+    return send_file(os.path.join(app.config['UPLOADED_FILES_DEST'], filename), as_attachment=True) 
 
 @app.route('/play/<filename>')
 def play(filename):
